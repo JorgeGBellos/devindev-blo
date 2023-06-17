848bf8979e2763885c136173e4b10d9a9c166da5
JorgeAGB <jorgegbellos@gmail.com>
2023-06-16 20:06:17 -0600

    Home page almost finished, blog posts added to test

---
+++ src/js/constants.js
@@ -0,0 +1,6 @@
+//Constants files
+export const GITHUB = "https://github.com/JorgeGBellos";
+
+export const WEBSITE = "https://devindev.com/";
+
+export const LINKEDIN = "https://www.linkedin.com/in/jorgeagb/";
